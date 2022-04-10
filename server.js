91b97b20b830e03ef203655e665a501f70e44b79
Tony Linz <tonyllinz@gmail.com>
2022-04-09 21:49:03 -0500

    update server.js to use heroku server if avail, otherwise localhost:27017

--- server.js
+++ server.js
@@ -16,14 +16,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    }
+);
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
